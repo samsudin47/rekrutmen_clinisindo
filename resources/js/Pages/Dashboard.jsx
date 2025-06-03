60f99b1b3849989072f4fbe5daa3500acc0cc930
samsudin <mohamadsamsudin47@gmail.com>
2025-06-03 16:43:58 +0700

    feat:add feature dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,12 +1,13 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
+import Cart from "../Pages/DashboardCart/Cart";
 
 export default function Dashboard() {
     return (
         <AuthenticatedLayout
             header={
                 <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    Dashboard
+                    Dashboard Cart
                 </h2>
             }
         >
@@ -16,7 +17,7 @@ export default function Dashboard() {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            You're logged in!
+                            <Cart />
                         </div>
                     </div>
                 </div>
